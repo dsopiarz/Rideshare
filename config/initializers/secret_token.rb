94643f628b3114d19f73501a39f9209158b1f314
Darren Sopiarz <dsopiarz@gmail.com>
2014-04-03 19:37:38 -0500

    "Initial upload - This is the default rails generated app"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rideshare::Application.config.secret_key_base = 'c080c7140b5ee64f0b1f9ae7581d40ca0756f6acb0fc029b91239b98210f23477421901cd00aab01e82aeac9ea28e80bb574fadccc6b260489b4e1092e90b500'
