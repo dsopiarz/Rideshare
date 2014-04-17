54c2f670a3eedf677dedefc2979268b619ad83a8
dbougie <arnnie55@gmail.com>
2014-04-17 13:55:27 -0500

    adds routing for navigation links for Home page and Post New Ride(new) page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rideshare::Application.routes.draw do
   resources :rides
+  
+  get "/new", :to => "rides#new", :as => "new"
+  root :to => "rides#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
