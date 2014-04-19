397d698c21a47b978c23b41548c556a1664969e1
Darren Sopiarz <dsopiarz@gmail.com>
2014-04-19 15:20:55 -0500

    Adds logic to build where clause string, also cleans up search partial

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,9 +1,28 @@
 class Ride < ActiveRecord::Base
   def self.search(params)
-    if params[:departlocation].present? && params[:arrivelocation].present? && params[:departtime].present?
-      where(["departlocation LIKE :dloc AND arrivelocation LIKE :aloc AND departtime LIKE :dtime", {dloc: "%#{params[:departlocation]}%", aloc: "%#{params[:arrivelocation]}%", dtime: "%#{params[:departtime]}%"}])
-	else
-	  all
-	end
+    if params[:departlocation].present? || params[:arrivelocation].present? || params[:departtime].present?
+    query = ""
+
+    if not params[:departlocation].blank?
+      query << "departlocation LIKE '%#{params[:departlocation]}%'"
+    end
+
+    if not params[:arrivelocation].blank?
+      if not query.blank?
+        query << " AND "
+      end
+      query << "arrivelocation LIKE '%#{params[:arrivelocation]}%'"
+    end
+
+    if not params[:departtime].blank?
+      if not query.blank?
+        query << " AND "
+      end
+      query << "departtime LIKE '%#{params[:departtime]}%'"
+    end
+      where(query)
+  else
+    all
+  end
   end
 end
