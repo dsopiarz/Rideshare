2a5e676699fd32f5357cbfc641f5b2418323ba8d
Darren Sopiarz <dsopiarz@gmail.com>
2014-04-22 12:13:48 -0500

    Fixes schema.rb had wrong version number.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422034426) do
+ActiveRecord::Schema.define(version: 20140422002057) do
 
   create_table "rides", force: true do |t|
     t.string   "departlocation"
