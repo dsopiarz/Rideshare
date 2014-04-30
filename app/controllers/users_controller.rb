ee18496faed56c5d014ed5347659616bb0f06f21
dbougie <arnnie55@gmail.com>
2014-04-29 23:11:11 -0500

    implements users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:destroy]
+   
+   
+  #get users
+  def users_results
+    @users = User.search(params)
+  end
+  
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to user_search_url }
+      format.json { head :no_content }
+    end
+  end
+   
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+	end
+	
+	# Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+	  params.require(:user).permit(:email)
+	end
+end
