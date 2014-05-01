59aa69f38570c686080e82e11d41158a8c9a8db3
Johnny Allegro <jallegro1@student.cvtc.edu>
2014-05-01 23:20:53 +0000

    adds method for creating the image src for gravatar users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
 
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@student\.cvtc\.edu\z/,
 			      message: "must be a CVTC student email account" }
+
+  def img_src
+    email_address = self.email.downcase.to_s.strip
+    hash = Digest::MD5.hexdigest(email_address.downcase.strip)
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
+
 end
